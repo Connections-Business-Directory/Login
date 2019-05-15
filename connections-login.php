dbaee573f363a6ddb2f17c5d6d6e02f9c684787a
Steven A. Zahm <shazahm1@hotmail.com>
2019-05-15 09:32:21 -0400

    Update copyright year.

--- connections-login.php
+++ connections-login.php
@@ -7,7 +7,7 @@
  * @author    Steven A. Zahm
  * @license   GPL-2.0+
  * @link      http://connections-pro.com
- * @copyright 2017 Steven A. Zahm
+ * @copyright 2019 Steven A. Zahm
  *
  * @wordpress-plugin
  * Plugin Name:       Connections Login
